cdc0577ae70648023bd051589beeb16a38edd0c0
Stepan Kozurak <stepan.kozurak@sas.com>
2022-06-01 00:30:32 +0200

    Added homework 11

---
+++ Praca_domowa_cw11.py
@@ -0,0 +1,100 @@
+import dash
+import numpy as np
+from dash import dcc, html, dash_table
+import pandas as pd
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model, tree
+import plotly.graph_objects as go
+
+app = dash.Dash()
+
+df = pd.read_csv('DATA/cw11/winequelity.csv')
+df_top_5 = df.head(5)
+
+
+def get_drop_down_items(values: list) -> list:
+    dropdown_items = []
+    for i in df_top_5.columns:
+        if i not in values:
+            dropdown_items.append(i)
+    return dropdown_items
+
+
+fnameDict = {
+    'Regression': get_drop_down_items(['Unnamed: 0', 'pH']),
+    'Classification': get_drop_down_items(['Unnamed: 0'])
+}
+
+models = {'Regression': linear_model.LinearRegression,
+          'Decision Tree': tree.DecisionTreeRegressor}
+
+names = list(fnameDict.keys())
+nestedOptions = fnameDict[names[0]]
+
+app.layout = html.Div(
+    [
+        html.Div([
+            html.P('Select chart type', style={}),
+            dcc.Dropdown(
+                id='name-dropdown',
+                options=[{'label': name, 'value': name} for name in names],
+                value=list(fnameDict.keys())[1]
+            ),
+        ], style={'width': '50%', 'display': 'inline-block'}),
+        html.Div([
+            html.P('Select values', style={}),
+            dcc.Dropdown(
+                id='opt-dropdown'
+            ),
+        ], style={'width': '50%', 'display': 'inline-block'}
+        ),
+        html.Hr(),
+        dash_table.DataTable(
+            df_top_5.to_dict('records'),
+            [{"name": i, "id": i} for i in df_top_5.columns],
+        ),
+        dcc.Graph(id="graph"),
+    ]
+)
+
+
+@app.callback(
+    [dash.dependencies.Output('opt-dropdown', 'options'),
+     dash.dependencies.Output('opt-dropdown', 'value')],  # setting default value for second dropdown
+    [dash.dependencies.Input('name-dropdown', 'value')]
+)
+def update_date_dropdown(name):
+    return [{'label': i, 'value': i} for i in fnameDict[name]], fnameDict[name][0]
+
+
+@app.callback(
+    dash.dependencies.Output("graph", "figure"),
+    [dash.dependencies.Input('name-dropdown', "value"),  # get input values from both dropdowns
+     dash.dependencies.Input('opt-dropdown', "value")]
+)
+def train_and_display(name, options):
+    if name == 'Regression':
+        X = df.pH.values[:, None]
+        X_train, X_test, y_train, y_test = train_test_split(X, df[options], random_state=10000)
+
+        model = models[name]()
+        model.fit(X_train, y_train)
+
+        x_range = np.linspace(X.min(), X.max(), 100)
+        y_range = model.predict(x_range.reshape(-1, 1))
+
+        fig = go.Figure([
+            go.Scatter(x=X_train.squeeze(), y=y_train,
+                       name='train', mode='markers'),
+            go.Scatter(x=X_test.squeeze(), y=y_test,
+                       name='test', mode='markers'),
+            go.Scatter(x=x_range, y=y_range,
+                       name='prediction')
+        ])
+        return fig
+    return px.pie(df[['target', options]], values=options, names='target')
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ README.md
@@ -0,0 +1,33 @@
+# Homeworks to PAD (Data Analysis Programing in Python) in PJATK
+
+## First steps
+
+- Create venv by typing
+
+```shell
+python3 -m venv venv_name
+```
+
+- Activate venv Windows
+
+```shell
+venv/Scripts/activate
+```
+
+- Activate venv UNIX
+
+```shell
+source venv/bin/activate
+```
+
+- Upgrade pip
+
+```shell
+python3 pip install --upgrade pip
+```
+
+- Install requrements from requirements.txt file
+
+```shell
+pip install -r requirements.txt
+```
